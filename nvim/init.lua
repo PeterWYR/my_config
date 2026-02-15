eb3861d3587c4ba572d5a4d71d0ef58c47de2172
PeterWYR <24372108@emails.bjut.edu.cn>
2026-02-15 17:38:25 +0800

    feat: nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -422,6 +422,22 @@ require('lazy').setup({
     end,
   },
 
+  -- nvim.dbee
+  {
+    'kndndrj/nvim-dbee',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+    },
+    build = function()
+      -- Install tries to automatically detect the install method.
+      -- if it fails, try calling it with one of these parameters:
+      --    "curl", "wget", "bitsadmin", "go"
+      require('dbee').install()
+    end,
+    config = function()
+      require('dbee').setup(--[[optional config]])
+    end,
+  },
   -- Filetree: Neo-tree
   {
     'nvim-neo-tree/neo-tree.nvim',
