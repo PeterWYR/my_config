b08daa1275eb30bcd2ce3fd7bb655e7fb99346dd
YiRan Wang <24372108@emails.bjut.edu.cn>
2026-07-20 11:09:31 +0800

    added neovim config

--- nvim/lua/config/autocmds.lua
+++
@@ -1,9 +0,0 @@
-local highlight_yank = vim.api.nvim_create_augroup("highlight-yank", { clear = true })
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-  group = highlight_yank,
-  desc = "Highlight text after yanking",
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
