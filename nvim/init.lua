eb358c59d8bad6f4e4691929cfa12f729f9ef509
YiRan Wang <24372108@emails.bjut.edu.cn>
2026-07-31 00:24:25 +0800

    update config

--- nvim/init.lua
+++ nvim/init.lua
@@ -391,26 +391,85 @@ do
   -- change the command under that to load whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  vim.g.gruvbox_italic_functions = true
-  vim.g.gruvbox_sidebars = { 'qf', 'vista_kind', 'terminal', 'packer' }
-
-  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-  vim.g.gruvbox_colors = { hint = 'orange', error = '#ff0000' }
-
-  -- Change the TabLineSel highlight group (used by barbar.nvim) to the "orange" color
-  vim.g.gruvbox_theme = { TabLineSel = { bg = 'orange' } }
-  vim.pack.add { gh 'eddyekofo94/gruvbox-flat.nvim' }
-  vim.g.gruvbox_flat_style = 'hard'
-  vim.cmd [[colorscheme gruvbox-flat]]
   -- vim.pack.add { gh 'neanias/everforest-nvim' }
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
   -- vim.g.gruvbox_flat_style = "hard"
+  vim.pack.add { { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' } }
 
-  vim.g.loaded_netrw = 1
-  vim.g.loaded_netrwPlugin = 1
+  require('catppuccin').setup {
+    flavour = 'auto', -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+      light = 'latte',
+      dark = 'mocha',
+    },
+    transparent_background = false, -- disables setting the background color.
+    float = {
+      transparent = false, -- enable transparent floating windows
+      solid = false, -- use solid styling for floating windows, see |winborder|
+    },
+    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+    dim_inactive = {
+      enabled = false, -- dims the background color of inactive window
+      shade = 'dark',
+      percentage = 0.15, -- percentage of the shade to apply to the inactive window
+    },
+    no_italic = false, -- Force no italic
+    no_bold = false, -- Force no bold
+    no_underline = false, -- Force no underline
+    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+      comments = { 'italic' }, -- Change the style of comments
+      conditionals = { 'italic' },
+      loops = {},
+      functions = {},
+      keywords = {},
+      strings = {},
+      variables = {},
+      numbers = {},
+      booleans = {},
+      properties = {},
+      types = {},
+      operators = {},
+      -- miscs = {}, -- Uncomment to turn off hard-coded styles
+    },
+    lsp_styles = { -- Handles the style of specific lsp hl groups (see `:h lsp-highlight`).
+      virtual_text = {
+        errors = { 'italic' },
+        hints = { 'italic' },
+        warnings = { 'italic' },
+        information = { 'italic' },
+        ok = { 'italic' },
+      },
+      underlines = {
+        errors = { 'underline' },
+        hints = { 'underline' },
+        warnings = { 'underline' },
+        information = { 'underline' },
+        ok = { 'underline' },
+      },
+      inlay_hints = {
+        background = true,
+      },
+    },
+    color_overrides = {},
+    custom_highlights = {},
+    auto_integrations = true,
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      nvimtree = true,
+      notify = false,
+      mini = {
+        enabled = true,
+        indentscope_color = '',
+      },
+      -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
+  }
 
+  -- setup must be called before loading
+  vim.cmd.colorscheme 'catppuccin-nvim'
   vim.pack.add {
     {
       src = 'https://github.com/nvim-lua/plenary.nvim',
